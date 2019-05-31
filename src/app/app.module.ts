3d24b4da8b9adf2ab99ab1e7befe9c8555debb96
dd270386pai <dd270386pai@gmail.com>
2019-05-31 09:23:02 +0300

    page not found

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NonExistingComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
