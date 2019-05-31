6d585d6f86670454aeabda59ea1420c925ec0ef6
dd270386pai <dd270386pai@gmail.com>
2019-05-31 12:22:01 +0300

    about/terms/privacy routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'terms-of-use', component: TermsOfUseComponent},
+  { path: 'privacy-policy', component: PrivacyPolicyComponent},
   { path: '**', component: PageNotFoundComponent }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AboutComponent } from './about/about.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NonExistingComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    AboutComponent,
+    TermsOfUseComponent,
+    PrivacyPolicyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
